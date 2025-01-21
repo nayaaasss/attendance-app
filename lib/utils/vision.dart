daab901470863957a6009a6c42286eab792c166d
nayaaasss <widamud0323@gmail.com>
2025-01-21 09:05:51 +0700

    initial config for face detection future

---
+++ lib/utils/vision.dart
@@ -0,0 +1,12 @@
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
+class Vision {
+  //pribvat constrauctor
+  Vision._();
+
+  static final Vision instance = Vision._();
+
+  FaceDetector faceDetector([FaceDetectorOptions? option]) {
+  return FaceDetector(options: option ?? FaceDetectorOptions());
+  }
+}
