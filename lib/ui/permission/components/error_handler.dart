78bda31ff85daf0095b8f80f5fc42d8a82321cc8
nayaaasss <widamud0323@gmail.com>
2025-02-05 11:09:06 +0700

    create error_handler

---
+++ lib/ui/permission/components/error_handler.dart
@@ -0,0 +1,13 @@
+import 'package:attendace_app/ui/permission/components/snack_bar.dart';
+import 'package:flutter/material.dart';
+
+class ErrorHandler {
+  static void handleError(BuildContext context, dynamic error) {
+    SnackBarComponent.showSnackBar(
+      context,
+      error,
+      isError: true
+      );
+      Navigator.of(context).pop();
+  }
+}
